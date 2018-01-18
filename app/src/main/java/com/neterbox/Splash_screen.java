475a5f6933d9bfc494b987db0039d4b964488acb
NeterBox <m.neterbox@gmail.com>
2018-01-18 11:02:44 +0530

    login success

--- app/src/main/java/com/neterbox/Splash_screen.java
+++ app/src/main/java/com/neterbox/Splash_screen.java
@@ -15,22 +15,6 @@ public class Splash_screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-//        Thread mythread = new Thread(){
-//            @Override
-//            public void run() {
-//                try {
-//                    sleep(1000);
-//                    Intent intent = new Intent(getApplicationContext(),LoginPage.class);
-//                    startActivity(intent);
-//                    finish();
-//                } catch (InterruptedException e) {
-//                    e.printStackTrace();
-//                }finally {
-//
-//                }
-//            }
-//        };
-//        mythread.start();
 
         new Handler().postDelayed(new Runnable(){
             @Override
